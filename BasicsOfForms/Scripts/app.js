52e43487d92481961a48d39a6e9874b49b64409f
JZorvan <jzorvan@gmail.com>
2016-11-05 06:37:46 -0700

    Steps 9 & 10, POST getting 404 error

--- BasicsOfForms/Scripts/app.js
+++ BasicsOfForms/Scripts/app.js
@@ -1,1 +1,11 @@
-﻿var app = angular.module("AppCtrl", []);
+﻿"use strict";
+
+var app = angular.module("App", []);
+
+app.controller("AppCtrl", function ($scope, $http) {
+    $scope.formSubmit = () => {
+        var form = $("#Form").serialize();
+        $http.post("/api/AngularForm", form).then(console.log(JSON.stringify(form)));
+        event.preventDefault();
+    };
+});
