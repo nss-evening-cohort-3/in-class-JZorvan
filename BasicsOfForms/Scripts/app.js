78d892bd88f4c915163a006150f8449f3fd9cc93
JZorvan <jzorvan@gmail.com>
2016-11-05 07:50:10 -0700

    Complete

--- BasicsOfForms/Scripts/app.js
+++ BasicsOfForms/Scripts/app.js
@@ -3,9 +3,15 @@
 var app = angular.module("App", []);
 
 app.controller("AppCtrl", function ($scope, $http) {
-    $scope.formSubmit = () => {
-        var form = $("#Form").serialize();
-        $http.post("/api/AngularForm", form).then(console.log(JSON.stringify(form)));
+    $scope.formSubmit = (user) => {
+
+        $http.post("/api/AngularApi", user)
+            .then(function (response) {
+                console.log("Posted successfully.")
+            }, function (error) {
+                console.log("That definitely didn't work.")
+            });
+        console.log(user);
         event.preventDefault();
     };
 });
